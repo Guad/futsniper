31e8e7a27e685d8e291e93a5df3a6c2da3cefc6f
Felipe Perez <felipe@chichasov.es>
2015-01-05 00:05:54 +0100

    Debugging purposes.

--- futsniper.py
+++ futsniper.py
@@ -46,11 +46,13 @@ while True:
 	percent = 100-((lowestCards[0]['buyNowPrice'] / float(lowestCards[1]['buyNowPrice']))*100)
 	averagePercent = 100-((lowestCards[0]['buyNowPrice'] / float(averageCost))*100)
 	print 'Possible profit is', str(percent)[:4] + '%', 'since the cheapest is', lowestCards[0]['buyNowPrice'], 'and the second is', lowestCards[1]['buyNowPrice']
+	print 'Average is ' + str(averageCost) + ' and its ' + str(averagePercent)[:4] + '% more costly.'
 	entry =  "=====LOG ENTRY" + strftime("%H:%M:%S") + "=====\n"
 	entry += "Inspected Player: " + player['Player'] + '\n'
 	entry += "Prices: " + str(lowestCards[0]['buyNowPrice']) + "(lowest), " + str(lowestCards[1]['buyNowPrice']) + ', ' + str(lowestCards[2]['buyNowPrice']) + '\n'
 	entry += "Threshold: " + futcore.settings['THRESHOLD'] +'\n'
 	entry += "Enough?: " + str(percent >= int(futcore.settings['THRESHOLD'])) + '\n'
+	entry += "Average price is " + str(averageCost) + ", it's " + str(averagePercent)[:4] + "% more costly"
 	logAction(entry)
 	if percent >= int(futcore.settings['THRESHOLD']) and averagePercent >= int(futcore.settings['THRESHOLD']) and lowestCards[0]['buyNowPrice'] > fifa.credits: #PROFIT!
 		if futcore.checkLowerPrice(fifa, player['ID'], lowestCards[0]['buyNowPrice']):
